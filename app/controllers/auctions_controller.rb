083b86be11e17d144a852354f45bfa0d34ac21c4
Colt <coltsteele1@gmail.com>
2014-05-12 10:56:07 -0700

    auction and bid forms working

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -2,4 +2,19 @@ class AuctionsController < ApplicationController
   def index
     @auctions = Auction.all
   end
+
+  def new
+    @auction = Auction.new
+  end
+
+  def create
+    # binding.pry
+    @auction = Auction.create(auction_params)
+    redirect_to auctions_path
+  end
+
+  private
+  def auction_params
+    params[:auction].permit(:title, :description, :bids_attributes=>[:amount])
+  end
 end
--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,10 +1,39 @@
 class BidsController < ApplicationController
+  before_filter :load_auction
   def index
-    @auction = Auction.find(params[:auction_id])
-    @bids = @auction.bids.all
+    @bids = @auction.bids.all.order(:amount)
   end
 
   def show
+    @bid = @auction.bids.find(params[:id])
+  end
+
+  def new
+    @bid = @auction.bids.new
+  end
+
+  def create
+    @bid = @auction.bids.new(bid_params)
+    if @bid.save
+      redirect_to auction_bids_path(@auction)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @bid = @auction.bids.find(params[:id])
+    @bid.destroy
+    redirect_to auction_bids_path(@auction)
+  end
+
+  private
+  def bid_params
+    params.require(:bid).permit(:amount)
+  end
+
+  def load_auction
+    @auction = Auction.find(params[:auction_id])
   end
 
 end
