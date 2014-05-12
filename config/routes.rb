8e7ba330df293bb6b7864d71d1d74e8601e4a8f3
Colt <coltsteele1@gmail.com>
2014-05-11 21:45:23 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :auctions do
+      resources :bids
+  end
+
+  root 'auctions#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
